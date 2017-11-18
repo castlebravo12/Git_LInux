94e5c9a7d003ee32feef290feb9bcfb05eaf7e3a
cstlbrvo <ppakora@gmail.com>
2017-11-19 02:09:09 +0530

    No changes

---
+++ src/com/company/Animal.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Created by pankaj on 18/11/17.
+ */
+public abstract class Animal {
+    public abstract void eat();
+    public abstract void kill();
+}
---
+++ src/com/company/Dog.java
@@ -0,0 +1,19 @@
+package com.company;
+
+/**
+ * Created by pankaj on 18/11/17.
+ */
+public class Dog extends Animal
+{
+    public void eat()
+    {
+        System.out.println("I love chicken");
+
+    }
+    public void kill()
+    {
+        System.out.println("I need CHicken else I will eat You");
+    }
+
+
+}
